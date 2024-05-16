cd1aa049969d986a34fde2e3088adc04cf1a694c
Rahul Choudhary <rahulchoudhary.bit@gmail.com>
2024-05-17 02:06:35 +0530

    Solid ends

--- app/src/main/java/solid/battleship/ships/Battleship.java
+++ app/src/main/java/solid/battleship/ships/Battleship.java
@@ -8,5 +8,6 @@ import java.util.List;
 public class Battleship extends Ship {
     public Battleship(ShipType shipType, List<GameMap> coordinates, Move move) {
         super(shipType, coordinates, move);
+
     }
 }
